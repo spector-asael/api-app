c0a64ab79b55fa99a94dddbbd9d297f1129d85a3
Asael Tobar <2023158061@ub.edu.bz>
2025-04-03 14:40:05 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const apiKey = "pwvp8t1Yt7r6BL14k56LLw==a3w85m1RDIdniGj9";
+const quoteApiUrl = "https://api.api-ninjas.com/v1/quotes";
+const quoteImageUrl = "https://random-image-pepebigotes.vercel.app/api/random-image";
