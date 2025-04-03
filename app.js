fe4ed5f76934630606811ab1215c60601ffda1ee
Asael Tobar <2023158061@ub.edu.bz>
2025-04-03 14:47:03 -0600

    Created the 2 promises

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const apiKey = "pwvp8t1Yt7r6BL14k56LLw==a3w85m1RDIdniGj9";
 const quoteApiUrl = "https://api.api-ninjas.com/v1/quotes";
 const quoteImageUrl = "https://random-image-pepebigotes.vercel.app/api/random-image";
+
+const getQuote = new Promise((resolve, reject) => {
+  fetch(quoteApiUrl, {
+    method: "GET",
+    headers: {
+      "X-API-Key": apiKey,
+    }
+  })
+  .then(data => {
+    console.log("API Response (Quote):", data);
+    resolve(data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+    reject(error);
+  })
+})
+
+const getImage = new Promise((resolve, reject) => {
+  fetch(quoteImageUrl)
+  .then(response => response.json()) // Parse JSON response
+  .then(data => {
+    console.log("Image URL:", data.imageUrl);
+    resolve(data.imageUrl); // Resolve with the image URL
+  })
+  .catch(error => {
+    console.error("Error fetching image:", error);
+    reject(error);
+  })
+});
